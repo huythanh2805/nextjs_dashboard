b28dcc2d2b331502f67110c98fbe7ab5f6d417e1
huythanh2805 <huythanhle2805@gmail.com>
2025-01-01 21:51:06 +0700

    Create main route, adding font for layout, create error and not-found page

---
+++ components/Invoices/Form.tsx
@@ -0,0 +1,9 @@
+
+type Props = {
+   userName: string
+}
+const InvoicesForm: React.FC<Props> = ({userName}) => {
+   
+   return <div>{userName}</div>
+}
+export default InvoicesForm
---
+++ lib/font.ts
@@ -0,0 +1,4 @@
+import {Inter, Poppins, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const poppins = Poppins({ weight: ['400', '500', '600'], subsets: ['latin'] });
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']});
